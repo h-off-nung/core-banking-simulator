5456dbcc931910306af59e0bfbfb1b94dbf573c2
Kirill Bobrov <100440636+h-off-nung@users.noreply.github.com>
2024-08-25 20:09:59 +0200

    Remove unnecessary methods

--- src/main/java/com/bobrov/model/User.java
+++ src/main/java/com/bobrov/model/User.java
@@ -9,15 +9,13 @@ import java.util.List;
 
 public class User {
     private String name;
-    private String surname;
-    private LocalDate birthday;
-    private String sex;
+    private final String surname;
+    private final LocalDate birthday;
+    private final String sex;
     private String username;
     private String password;
-    private int isAdmin = 0;
     private final List<Card> cards;
 
-    // Constructor
     public User(String name, String surname, LocalDate birthday, String sex, String username, String password) {
         this.name = name;
         this.surname = surname;
@@ -27,8 +25,6 @@ public class User {
         this.password = password;
         this.cards = new ArrayList<>();
     }
-
-    // Getters and setters
 
     public String getName() {
         return name;
@@ -43,27 +39,15 @@ public class User {
     }
 
     public int getIsAdmin() {
-        return isAdmin;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
+        return 0;
     }
 
     public LocalDate getBirthday() {
         return birthday;
     }
 
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
     public String getSex() {
         return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
     }
 
     public String getUsername() {
@@ -94,38 +78,8 @@ public class User {
         }
     }
 
-    // Method to perform a transaction
-    public void performTransaction(Transaction transaction) {
-        // Implement transaction logic here, such as checking daily limit, etc.
-    }
-
-    // Method to view user's transactions
-    public List<Transaction> getTransactions() {
-        List<Transaction> transactions = new ArrayList<>();
-        for (Card card : this.cards) {
-            transactions.addAll(card.getTransactions());
-        }
-        return transactions;
-    }
-
-    // Method to view statistics for the transactions
-    public String getStatistics() {
-        // Implement statistics logic here, like amount and transactions number of a month
-        return "Statistics";
-    }
-
-    // Method to sign up a new user (static factory method)
     public static User signUp(String name, String surname, LocalDate birthday, String sex, String username, String password) {
         return new User(name, surname, birthday, sex, username, password);
-    }
-
-    // Method to log in an existing user
-    public static User logIn(String username, String password, UserService userService) throws SQLException {
-        User user = userService.authenticateUser(username, password);
-        if (user == null) {
-            throw new IllegalArgumentException("Invalid username or password.");
-        }
-        return user;
     }
 }
 
