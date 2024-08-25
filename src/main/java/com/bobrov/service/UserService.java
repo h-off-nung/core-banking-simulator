a992732c45a3daff4f687e91c61b17dbc79f98cb
Kirill Bobrov <100440636+h-off-nung@users.noreply.github.com>
2024-08-25 18:43:44 +0200

    Change the name of the targeted table and add the isAdmin attribute to the createUser method

--- src/main/java/com/bobrov/service/UserService.java
+++ src/main/java/com/bobrov/service/UserService.java
@@ -19,20 +19,21 @@ public class UserService {
 
     // Method to create a new user
     public void createUser(User user) throws SQLException {
-        String sql = "INSERT INTO users (name, surname, birthday, sex, username, password) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO people (name, surname, birthday, sex, username, password, isAdmin) VALUES (?, ?, ?, ?, ?, ?, ?)";
         databaseService.executePreparedUpdate(sql,
                 user.getName(),
                 user.getSurname(),
                 user.getBirthday().toString(),
                 user.getSex(),
                 user.getUsername(),
-                user.getPassword());
+                user.getPassword(),
+                user.getIsAdmin());
         System.out.println("User " + user.getUsername() + " created successfully.");
     }
 
     // Method to authenticate a user
     public User authenticateUser(String username, String password) throws SQLException {
-        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        String sql = "SELECT * FROM people WHERE username = ? AND password = ?";
         ResultSet resultSet = databaseService.executePreparedQuery(sql, username, password);
 
         if (resultSet.next()) {
@@ -51,7 +52,7 @@ public class UserService {
 
     // Method to update a user's information
     public void updateUser(User user) throws SQLException {
-        String sql = "UPDATE users SET name = ?, surname = ?, birthday = ?, sex = ?, password = ? WHERE username = ?";
+        String sql = "UPDATE people SET name = ?, surname = ?, birthday = ?, sex = ?, password = ? WHERE username = ?";
         databaseService.executePreparedUpdate(sql,
                 user.getName(),
                 user.getSurname(),
@@ -65,7 +66,7 @@ public class UserService {
     // Method to retrieve all users
     public List<User> getAllUsers() throws SQLException {
         List<User> users = new ArrayList<>();
-        String sql = "SELECT * FROM users";
+        String sql = "SELECT * FROM people WHERE isAdmin = 0";
         ResultSet resultSet = databaseService.executeQuery(sql);
 
         while (resultSet.next()) {
@@ -84,7 +85,7 @@ public class UserService {
 
     // Method to find a user by username
     public User getUserByUsername(String username) throws SQLException {
-        String sql = "SELECT * FROM users WHERE username = ?";
+        String sql = "SELECT * FROM people WHERE username = ?";
         ResultSet resultSet = databaseService.executePreparedQuery(sql, username);
 
         if (resultSet.next()) {
