9d1c3a68c9c120606b4f336a0c1a2d869c1392db
Kirill Bobrov <100440636+h-off-nung@users.noreply.github.com>
2024-08-26 13:44:14 +0200

    Remove unused packages

--- src/main/java/com/bobrov/model/Card.java
+++ src/main/java/com/bobrov/model/Card.java
@@ -1,9 +1,7 @@
 package com.bobrov.model;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Card {
